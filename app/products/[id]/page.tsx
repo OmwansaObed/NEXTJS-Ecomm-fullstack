09fce8548b9b2316c70d0fe437fdc19162c249f8
OmwansaObed <obed9to5@gmail.com>
2025-04-05 20:51:17 +0300

    Fixed issues

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,14 +1,9 @@
 import ProductDetails from "@/components/product-detail";
 import { stripe } from "@/lib/stripe";
 
-const ProductPage = async ({
-  params,
-}: {
-  params: {
-    id: string;
-  };
-}) => {
-  const product = await stripe.products.retrieve(params.id, {
+const ProductPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  const product = await stripe.products.retrieve(id, {
     expand: ["default_price"],
   });
   return <ProductDetails product={product} />;
